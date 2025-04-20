14ccca3c25c4a5bb8f053ce079d13cb6a7848101
Kirill <asefru78@mail.ru>
2025-04-20 22:20:59 +0300

    Reverse order of creating kr

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -ex
 # set up sharding
 psql -h $PG_HOST -p $PG_PORT -U spqr-console -c "CREATE DISTRIBUTION ds1 COLUMN TYPES INTEGER"
 psql -h $PG_HOST -p $PG_PORT -U spqr-console -c "ALTER DISTRIBUTION ds1 ATTACH RELATION person DISTRIBUTION KEY id"
-psql -h $PG_HOST -p $PG_PORT -U spqr-console -c "CREATE KEY RANGE kr1 FROM 0 ROUTE TO sh1"
 psql -h $PG_HOST -p $PG_PORT -U spqr-console -c "CREATE KEY RANGE kr2 FROM 10 ROUTE TO sh2"
+psql -h $PG_HOST -p $PG_PORT -U spqr-console -c "CREATE KEY RANGE kr1 FROM 0 ROUTE TO sh1"
 
 # test
 mvn test -DpgHost=$PG_HOST -DpgPort=$PG_PORT -DpgDb=$PG_DATABASE -DpgUser=$PG_USER -DpgPassword=$PGPASSWORD
